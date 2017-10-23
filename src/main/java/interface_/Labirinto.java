6e63297b65c24eb6a0b05b6ce53af7cdb5405520
G. Panza <gaPanza@users.noreply.github.com>
2017-10-23 19:40:52 -0200

    Pega ai

--- src/main/java/interface_/Labirinto.java
+++ src/main/java/interface_/Labirinto.java
@@ -10,15 +10,18 @@ public class Labirinto {
 	private static String chessboard[][];
 	private static Integer x1;
 	private static Integer y1;
+	private static Integer size;
+
 	public static void main(String[] args) throws IOException {
 		Tabuleiro tabuleiro = new Tabuleiro();
 		Scanner scanner = new Scanner(System.in);
 		Service service = new Service();
-		System.out.println("Digite tamanho do tabuleiro numero de linhas = numero de colunas");
-		tabuleiro.setSize(15);
 
-		System.out.println("Digite quantidade de obstáculos");
-		tabuleiro.setObstaculos(scanner.nextInt());
+		//System.out.println("Digite tamanho do tabuleiro numero de linhas = numero de colunas");
+		tabuleiro.setSize(15);
+		size = tabuleiro.getSize() - 1;
+		//System.out.println("Digite quantidade de obstáculos");
+		tabuleiro.setObstaculos(20);
 
 		/*
 		 * #############################################################################
@@ -41,10 +44,11 @@ public class Labirinto {
 			int theNumberY = service.randomize();
 
 			if (((theNumberX == 0 && theNumberY == 0)
-					|| (theNumberX == tabuleiro.getSize() - 1 && theNumberY == tabuleiro.getSize() - 1))) {
+					|| (theNumberX == tabuleiro.getSize() - 1 && theNumberY == tabuleiro.getSize() - 1)
+					|| chessboard[theNumberX][theNumberY].equals("B"))) {
 				obst = obst - 1;
 			} else {
-				chessboard[theNumberX][theNumberY] = 	"B";
+				chessboard[theNumberX][theNumberY] = "B";
 
 			}
 
@@ -72,7 +76,7 @@ public class Labirinto {
 
 	public static void walk(Integer x, Integer y) throws IOException {
 		try {
-			Thread.sleep(1000);
+			Thread.sleep(500);
 			for (int i = 0; i < 50; ++i)
 				System.out.println();
 		} catch (InterruptedException e) {
@@ -80,7 +84,7 @@ public class Labirinto {
 		}
 
 		chessboard[x][y] = "X";
-		
+
 		for (int i = 0; i < chessboard.length; i++) {
 			System.out.println();
 
@@ -91,53 +95,52 @@ public class Labirinto {
 			System.out.print(" |");
 			System.out.println();
 		}
-		bestWay(x,y);
+		if (x == 14 && y == 14) {
+			main(null);
+		}
+		bestWay(x1, y1);
 	}
 
 	private static void bestWay(Integer x, Integer y) throws IOException {
 		double result = 0.0;
-		
-		result = result (x+1,y+1, result);
-		result = result (x+1,y+0, result);
-		result = result (x+0,y+1, result);
-		result = result (x+1,y-1, result);
-		result = result (x-1,y+1, result);
-		
-		
-		walk(x1,y1);
+
+		result = result(x + 1, y + 1, result);
+		result = result(x + 1, y + 0, result);
+		result = result(x + 0, y + 1, result);
+		result = result(x + 1, y - 1, result);
+		result = result(x - 1, y + 1, result);
+
+		walk(x1, y1);
 	}
-	
+
 	private static double result(Integer x, Integer y, double result) {
 		int h = 0;
-		
-		if(x < 0 || y<0) {
-			return 0.0;
+
+		if (x < 0 || y < 0) {
+			return result;
 		}
-		if(chessboard[x][y].equals("B")) {
-			return 0.0;
+		if (chessboard[x][y].equals("B")) {
+			return result;
 		}
-		
-		if(x<y) {
-			h = x + (y-x);
+
+		if (x < y) {
+			h = (size - y) + (y - x);
 		}
-		if(y<x) {
-			h = y + (x-y);
+		if (y < x) {
+			h = (size - x) + (x - y);
 		}
-		if(x == y) {
-			h = x - y;
+		if (x == y) {
+			h = (size - x) + (y - x);
 		}
-		double compare =  (14 - h)  / 14;
-		
-		if(compare > result) {
+		Double compare = (Double) (14.0 - h) / 14.0;
+
+		if (compare >= result) {
 			x1 = x;
 			y1 = y;
 		}
-		
+
 		return result > compare ? result : compare;
-		
-		
+
 	}
-	
-	
 
 }
